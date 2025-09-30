59dc51dcc129ef8bbfe8406912023487ec8c093e
AdityaP183 <adityaprasad1828@gmail.com>
2025-09-30 22:14:33 +0530

    fix(env): load app environment variables correctly
    - Ensure that environment variables are loaded properly in the app.
    - Also pushed updated Prisma schema to the database and modified npm
    scripts for consistency.

--- TODO.md
+++ TODO.md
@@ -2,9 +2,11 @@
 
 # UI Realted
 - Extract and put all the types from the apps into a shared package.
+- Fix theme switcher btn hydration error
 
 # Build Related
 - Adding proper eslint and formatting scripts to the tooling package and apps.
+- Fix scripts and remove unwanted
 
 # Runtime Related
 - Added the functionality to generate the prisma client(db) on build.
--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from "@skimr/ui/components/button";
-
 export default function Page() {
 	return (
 		<div className="flex min-h-svh items-center justify-center">
-			<div className="flex flex-col items-center justify-center gap-4">
-				<h1 className="text-2xl font-bold">Hello World</h1>
-				<Button size="sm">Button</Button>
-			</div>
+			<div className="flex flex-col items-center justify-center gap-4"></div>
 		</div>
 	);
 }
