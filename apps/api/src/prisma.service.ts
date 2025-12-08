6f3bb50f604690cc7baf62f4990345426669607c
AdityaP183 <adityaprasad1828@gmail.com>
2025-12-08 18:48:40 +0530

    feat(api-auth/init): better-auth setup with prisma and api setup

--- apps/api/src/prisma.service.ts
+++ apps/api/src/prisma.service.ts
@@ -1,10 +1,18 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "generated/prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+	extends PrismaClient
+	implements OnModuleInit, OnModuleDestroy
+{
 	async onModuleInit() {
-		console.log("Prisma client loaded...");
+		console.log("Prisma client connected...");
 		await this.$connect();
+	}
+
+	async onModuleDestroy() {
+		console.log("Prisma client disconnected...");
+		await this.$disconnect();
 	}
 }
