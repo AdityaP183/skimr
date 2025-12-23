164e4d5017e7e915f39efc769f992d58614d1eae
AdityaP183 <adityaprasad1828@gmail.com>
2025-12-23 21:13:08 +0530

    feat(api/auth): add user info and sessions view routes

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-	getHello(): string {
-		return "Hello World!";
+	getHealthCheck() {
+		return {
+			status: "ok",
+			message: "Skimr API is alive and running",
+			timestamp: Date.now(),
+		};
 	}
 }
